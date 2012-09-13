b2ea0b7240f12620930291ebbc75cb7277613d75
Sascha <sascha@535U4C.(none)>
2012-09-13 11:21:12 +0200

    Projekt Rohdaten

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyCalendar::Application.config.secret_token = 'f69fa8f1399ada418922a29f8231e4c40090a8f889742b1422e3b293f497f81b703233b268aac520f7805504366b8915a9c3b91e70001ca01ca09d19c9da946c'
